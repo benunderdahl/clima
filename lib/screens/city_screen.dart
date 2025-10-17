7d09107bbbd04e49b52af79dadf6053957d06249
Code Classroom <benjamin.underdahl@proton.me>
2025-10-16 23:27:30 -0500

    text widget in city screen

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -33,7 +33,18 @@ class _CityScreenState extends State<CityScreen> {
               ),
               Container(
                 padding: EdgeInsets.all(20.0),
-                child: null,
+                child: TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    icon: Icon(Icons.location_city),
+                    iconColor: Colors.white,
+                    hintText: "enter city name",
+                    hintStyle: TextStyle(
+                      color: Colors.grey
+                    ),
+                  ),
+                ),
               ),
               TextButton(
                 onPressed: () {},
